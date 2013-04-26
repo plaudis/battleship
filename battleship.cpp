43cef5116adc7fab5d54c9898edee7ac4663b7c9
plaudis <rplaudis@aol.com>
2013-04-25 23:17:53 -0300

    Update battleship.cpp

--- battleship.cpp
+++ battleship.cpp
@@ -4,10 +4,33 @@
  * 		Markus (m50) Clardy
  * 		Roberts Plaudis
  */
+#include <iostream>
 
-
+using namespace std;
 
 int main () {
-
+	cout<<".................. . ..... . ..... . . ... . . ... . . ... ...~DDD............~."<<endl\
+		<<"............................................................+DDD. .........:DDD."<<endl\
+		<<".................... ....... ..... . ..... . ..... . ....,DDDD.... .....IDND8. ."<<endl\
+		<<".......................................................~NDDD:.......,$DDDD......"<<endl\
+ 		<<".................................. . ..... . ..... ..:NDDD=......:8DDDD+........"<<endl\
+		<<".................. ....... ............... ........+DDDD~ ....:DNDDDN. ........."<<endl\
+		<<"............N?...................................DNDDD~....:8NDDDN.............."<<endl\
+		<<"............N?................................,NDDDD=...,ONDDDD................."<<endl\
+		<<"..........~:NI::............................:DDDDD:..,7DDDDD~..................."<<endl\
+		<<"   .........N?...... ....... ..... . .....:DDDDD+..~DDDDD8. ......,~::,........."<<endl\
+		<<" ........ DDDDDD........................~DDDDD:.~DDDDDNNDNDNDDDDDDDDDDDDDDDDN, ."<<endl\
+		<<" . .........N?....................... :NDDDD.~DNDDDDDDDDDDDDDDDDDDDDDDDDDD$~ ..."<<endl\
+		<<" .........NDDDDD......... ....... ..~DDDDN=NDDDDDDDDDDDDDDDDDDDDDDDDDDDD=::....."<<endl\
+		<<"..........~DDDD...................~DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD$:: ......"<<endl\
+		<<" .........DDDDDD................+DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD~::........"<<endl\
+ 		<<" ........DDDDDDDD............DNDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD+:::........."<<endl\
+ 		<<"........ DDDDDDDD....... NNDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD:::..........."<<endl\
+		<<" .......$DDDDDDDDNDD.DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD:::............"<<endl\
+		<<" .... DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD:::............."<<endl\
+		<<" ....NDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD:::.............."<<endl\
+		<<" ...NDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD:::,.............."<<endl\
+		<<" .NddddDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD::::..............."<<endl\
+		<<" NNDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDN::::. ............."<<endl;
 	return 0;
 }
